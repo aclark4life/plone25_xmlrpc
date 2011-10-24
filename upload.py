9910d98c77ac48a4e72f1adf4dbdd807bd88a4c5
Alex Clark <aclark@aclark.net>
2011-10-24 11:22:29 -0400

    This should probably work, but it doesn't

--- upload.py
+++ upload.py
@@ -1,19 +1,17 @@
-from OFS import Image
 import xmlrpclib
 
 filename = 'screenshot.png'
 mimetype = 'application/octet-stream'
 
 try:
-    proxy = xmlrpclib.ServerProxy('http://admin:admin@localhost:8080/Plone')
+    proxy = xmlrpclib.ServerProxy('http://admin:admin@localhost:8080/Plone', verbose=True)
     proxy.invokeFactory('Image', filename)
 except:
     pass
 
 try:
-    proxy = xmlrpclib.ServerProxy('http://localhost:8080/Plone/screenshot.png')
-    infile = open('screenshot.png')
-    data = File(filename, filename, infile, mimetype)
+    proxy = xmlrpclib.ServerProxy('http://localhost:8080/Plone/screenshot.png', verbose=True)
+    data = open('screenshot.png').read()
     proxy.setImage(data)
 except:
     pass
